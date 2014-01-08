f56285464299b4a58cf40c03f6bb7a878c811fe6
David Fedele <dave.fedele@gmail.com>
2014-01-08 01:49:42 -0800

    added show category

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,6 @@
 Video.create(title: "Futurama",description: "Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series. After he gets a job at an interplanetary delivery service, Fry embarks on ridiculous escapades to make sense of his predicament.", small_cover_url: "/tmp/futurama.jpg")
 Video.create(title: "South Park", description: "Follows the misadventures of four irreverent grade-schoolers in the quiet, dysfunctional town of South Park, Colorado.", small_cover_url: "/tmp/south_park.jpg")
 Video.create(title: "Family Guy", description: "In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.", small_cover_url: "/tmp/family_guy.jpg")
+Category.create(name: "TV Comedy")
+Category.create(name: "Horror")
+Category.create(name: "Documentary")

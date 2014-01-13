f27ae684a816ab98e39ef61e5fbf2223e877bdc6
David Fedele <dave.fedele@gmail.com>
2014-01-12 22:09:05 -0800

    added validations to video and category models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,10 @@
 class Category < ActiveRecord::Base
   has_many  :videos
 
+  validates :name, presence: true
+
   before_save :generate_slug
-  
+
   def generate_slug
     self.slug = self.name.gsub(' ', '-').downcase
   end

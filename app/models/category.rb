f666effdfb215b0b11d3261c6737d1376863f98e
David Fedele <dave.fedele@gmail.com>
2014-01-13 00:55:45 -0800

    added uniqueness validation to category model and create appropriate test

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
   has_many  :videos
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   before_save :generate_slug
 
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Category do
   it { should have_many(:videos)}
+  it { should validate_uniqueness_of(:name)}
 end

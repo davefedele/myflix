392e2ac593ec8e5f8ada1f776f4d3f08c8a217c6
David Fedele <dave.fedele@gmail.com>
2014-01-19 14:26:26 -0800

    added authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController 
+  def new
+    redirect_to home_path if current_user
+    @user = User.new
+  end
+
+  def create
+    user = User.where(email: params[:user][:email]).first
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to home_path
+      flash[:success] = "You are now signed in, enjoy!"
+    else
+      flash[:danger] = "Invalid email or password."
+      redirect_to sign_in_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+    flash[:warning] = "You have been signed out."
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Myflix::Application.routes.draw do
   root to: 'pages#front'
+  get '/home', to: 'videos#index'
+  
   resources :videos, only: [:show] do
     collection do
       post 'search', to: "videos#search"
     end
   end
+  
   get 'ui(/:action)', controller: 'ui'
   get '/register', to: 'users#new'
   get '/sign_in', to: 'sessions#new'
+  get '/sign_out', to: 'sessions#destroy'
+
   resources :users
   resources :categories, only: [:show]
+  resources :sessions, only: [:create]
 end

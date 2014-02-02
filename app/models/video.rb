b6ab4a3313c8a09ccc3bd681641c8d763dbd9ce6
David Fedele <dave.fedele@gmail.com>
2014-02-01 23:47:21 -0800

    added rating to view and video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -7,4 +7,13 @@ class Video < ActiveRecord::Base
     return [] if search_term.blank?
     where("title LIKE ?", "%#{search_term}%").order("created_at DESC")
   end
+
+  def avg_rating
+    return 0 if self.reviews.empty?
+    cumulative_rating = 0
+    self.reviews.each do |review|
+      cumulative_rating += review.rating
+    end
+    cumulative_rating/self.reviews.count.to_f
+  end
 end

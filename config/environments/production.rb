d59bc8db4143fcde7e06372815d2636a0485cc9d
David Fedele <dave.fedele@gmail.com>
2014-04-08 00:33:00 -0700

    updating env variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Myflix::Application.configure do
     domain: "example.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
-}
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD']
+  }
 end

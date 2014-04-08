441e72938865e36342fc2d23c2aa4074d4fc6177
David Fedele <dave.fedele@gmail.com>
2014-04-08 00:03:03 -0700

    added user welcome email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+ class UsersController < ApplicationController
   before_filter :require_user, only: [:show]
 
   def new
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      AppMailer.send_welcome_email(@user).deliver
       redirect_to sign_in_path
       flash[:success] = "You have been successfully registered. Please login"
     else
